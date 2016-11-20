adacecf0865dd4ba773ec5e6d5a8824e51733a35
alan pavicic <aka@svrg.net>
2016-11-20 20:55:13 +0100

    code structure improvements
    two things are added - tasks yield a 'reason' together with an id and
    task resume functionality is formalized and separated.
    
    at he moment the 'reason' why a task yields can be only 'async', but
    'chan' will be added soon

--- go.lua
+++ go.lua
@@ -2,44 +2,51 @@ local gfd = 0
 local function wait()
    gfd = gfd + 1
    print(gfd)
-   coroutine.yield(gfd)
+   coroutine.yield('async', gfd)
 end
 
 ------
-
-local pending_tasks = {}
-local tasks = {}
 
 local function thunk(f, ...)
    local args = { ... }
    return function () return f(unpack(args)) end
 end
 
+local pending_tasks = {}
+
 local function go(task, ...)
    local f = thunk(task, ...)
    table.insert(pending_tasks, coroutine.create(f))
 end
 
+local async_tasks = {}
+local function resume(task)
+   local ok, type, key = coroutine.resume(task)
+   if not ok then return end -- task throwed an exception
+   if coroutine.status(task) == 'dead' then return end
+   if type == 'async' then async_tasks[key] = task end
+end
+
 local function run_pending()
    for _, task in ipairs(pending_tasks) do
-      local _, fd = coroutine.resume(task)
-      print(fd)
-      tasks[fd] = task
+      resume(task)
    end
    pending_tasks = {}
+end
+
+local function async_wait()
+   local fd = io.read("*n") --select
+   local task = async_tasks[fd]
+   if task then
+      async_tasks[fd] = nil
+      resume(task)
+   end
 end
 
 local function run(f)
    local function loop()
       run_pending()
-      local fd = io.read("*n") --select
-      local task = tasks[fd]
-      if task then
-	 tasks[fd] = nil
-	 local _, fd = coroutine.resume(task)
-	 local status = coroutine.status(task)
-	 if status == 'suspended' then tasks[fd] = task end
-      end
+      async_wait()
       loop()
    end
    
@@ -47,7 +54,7 @@ local function run(f)
    loop()
 end
 
-------
+------ test -----
 local function f2(x, y)
    print(string.format("f2: %d %d", x, y))
    while true do
